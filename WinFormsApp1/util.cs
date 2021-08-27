606074dcc1f26069e4c8ed6363eb47c3eea4032d
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-08-27 13:18:10 -0500

    added comments and remove uneeded code

--- WinFormsApp1/util.cs
+++ WinFormsApp1/util.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 
+/**
+ * This class contains thev backend in order for this program to work
+ * It contains methods to calulate from C to F and F to C
+ * 
+ * 
+ * 
+ * 
+ */
+
 public class Class1
 {
-	public static void Method2()
-	{
-
-		Console.WriteLine("test");
 
 
-	}
-
-
-
+	/**
+	 * This method calculates from C to  F
+	 * @param C the celcius used to calculate to F
+	 * @return the results 
+	 * 
+	 */
 	public static double calcF(double C)
 	{
 		double result;
-
 		result = 1.8 * C + 32;
 		return result;
 
@@ -24,7 +30,6 @@ public class Class1
 	public static double calcC(double F)
 	{
 		double result;
-
 		result =  1.8 * F - 32;
 		return result;
 
